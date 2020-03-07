84f1fba01b318ced63d5e8756c2faa2bdbcf219f
Dorival Pedroso <dorival.pedroso@gmail.com>
2020-03-07 18:07:24 +1000

    Try some scroll avoid comps

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { GateStore } from '@cpmech/gate';
 import { GateSignUpView } from '../components';
 import { gate } from '../service';
 import { colors, params } from '../styles';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const s = StyleSheet.create({
   root: {
@@ -19,7 +21,9 @@ const s = StyleSheet.create({
   wrapper: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'center',
+    // justifyContent: 'center',
+    // justifyContent: 'flex-end',
+    minHeight: 800,
   },
   content: {
     flex: 1,
@@ -31,35 +35,39 @@ const s = StyleSheet.create({
 });
 
 export const SignUpScreen: React.FC = () => (
-  <View style={s.root}>
-    <View style={s.wrapper}>
-      <View style={s.content}>
-        <GateSignUpView
-          gate={gate as GateStore}
-          // colorButtonBg="#ffffff"
-          // colorButtonFg={colors.orange}
-          colorEye={colors.lightenOrange50pct}
-          colorText="#ffffff"
-          colorError="#ff1919"
-          styleInput={{
-            color: '#ffffff',
-            bgColor: colors.orange,
-            borderColor: colors.lightenOrange50pct,
-            hlColor: '#ffffff',
-            mutedColor: colors.lightenOrange50pct,
-            errorColor: '#ff1919',
-          }}
-          styleButton={{
-            color: colors.orange,
-            backgroundColor: '#ffffff',
-            fontWeight: 'bold',
-          }}
-          styleLink={{
-            color: 'yellow',
-            darkBackground: true,
-          }}
-        />
+  <KeyboardAwareScrollView>
+    <ScrollView>
+      <View style={s.root}>
+        <View style={s.wrapper}>
+          <View style={s.content}>
+            <GateSignUpView
+              gate={gate as GateStore}
+              // colorButtonBg="#ffffff"
+              // colorButtonFg={colors.orange}
+              colorEye={colors.lightenOrange50pct}
+              colorText="#ffffff"
+              colorError="#ff1919"
+              styleInput={{
+                color: '#ffffff',
+                bgColor: colors.orange,
+                borderColor: colors.lightenOrange50pct,
+                hlColor: '#ffffff',
+                mutedColor: colors.lightenOrange50pct,
+                errorColor: '#ff1919',
+              }}
+              styleButton={{
+                color: colors.orange,
+                backgroundColor: '#ffffff',
+                fontWeight: 'bold',
+              }}
+              styleLink={{
+                color: 'yellow',
+                darkBackground: true,
+              }}
+            />
+          </View>
+        </View>
       </View>
-    </View>
-  </View>
+    </ScrollView>
+  </KeyboardAwareScrollView>
 );
